ce0c017e25185d37ade469d93a1d9d1b359c3713
EditaGharibyan <editagharibyan.tt219-1@polytechnic.am>
2024-10-13 12:06:32 +0400

    Update RLE.cpp
    Add commit

--- RLE.cpp
+++ RLE.cpp
@@ -1,10 +1,74 @@
 #include <iostream>
+#include <string>
 using namespace std;
-class RLE{
+class RLE {
+private:
+    string info;
+    string encrypted;
+    string decrypted;
+public:
+    RLE() {}
 
+    RLE(string input) {
+        info = input;
+    }
+
+    void encrypt() {
+        encrypted = "";
+        int i = 0;
+        while (i < info.length()) {
+            int count = 1;
+            for (int j = i + 1; j < info.length(); j++) {
+                if (info[i] == info[j]) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+            encrypted += info[i];
+            encrypted += to_string(count);
+            i += count;
+        }
+    }
+
+    void decrypt(string encryptedText) {
+        decrypted = "";
+        int i = 0;
+        while (i < encryptedText.length()) {
+            char currentChar = encryptedText[i];
+            i++;
+            string countStr;
+            while (i < encryptedText.length() && isdigit(encryptedText[i])) {
+                countStr += encryptedText[i];
+                i++;
+            }
+            int count = stoi(countStr);  
+            for (int j = 0; j < count; j++) {
+                decrypted += currentChar;
+            }
+        }
+    }
+    void displayEncrypted() {
+        cout << "Encrypted text is: " << encrypted << endl;
+    }
+    void displayDecrypted() {
+        cout << "Decrypted text is: " << decrypted << endl;
+    }
 };
+
 int main() {
-    cout << "Try programiz.pro";
+    string input, encryptedInput;
+    cout << "Enter a string to encrypt: ";
+    cin >> input;
+    RLE rle(input);
+    rle.encrypt();
+    rle.displayEncrypted();
+    cout << "Enter an encrypted string to decrypt: ";
+    cin >> encryptedInput;
+
+    RLE decryptor;
+    decryptor.decrypt(encryptedInput);
+    decryptor.displayDecrypted();
 
     return 0;
 }
